55146db700d51edb31e194415fc3e10ee2204d1e
Sahil Ahmed <sahilahmed466@gmail.com>
2024-11-03 18:17:38 +0530

    client: setting up directories

---
+++ codestash-client/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        ubuntu: ["Ubuntu Condensed", "serif"],
+        raleway: ["Raleway", "serif"],
+        poppins: ["Poppins", "serif"],
+        openSans: ["Open Sans", "serif"],
+        mukta: ["Mukta", "serif"],
+        kanit: ["Kanit", "serif"],
+        inter: ["Inter", "serif"],
+      },
+    },
+  },
+  plugins: [],
+}
